f39f9ab7551412e2c0d81a8886abeaef2f5368df
hrithikD9 <hrithikdevnath0924@gmail.com>
2025-11-26 21:37:34 +0600

    backlog-1

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        {/* left section */}
+        <div>
+          <img className='mb-5 w-40 ' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Ipsam debitis perspiciatis necessitatibus impedit beatae odit architecto culpa,
+             dolorum deleniti nemo maxime, exercitationem blanditiis cum repudiandae dolores. 
+             Deleniti voluptate adipisci temporibus?</p>
+        </div>
+
+        {/* Center section */}
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Privacy Policy</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+        {/* right section */}
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+8801900001009</li>
+            <li>abc@example.com</li>
+          </ul>
+        </div>
+       </div>
+       {/*------Copyright text-------- */}
+       <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>© 2024 Prescripto. All rights reserved.</p>
+       </div>
+    </div>
+  )
+}
+
+export default Footer
